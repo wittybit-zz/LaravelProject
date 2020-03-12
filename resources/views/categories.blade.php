6b6fc1039b250c32964a8912d56f50053c383a5d
wittybit <vasurana142@gmail.com>
2020-03-12 10:24:15 +0530

    Started work on categories page

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -10,6 +10,121 @@
     @section('title','Categories')
     @section('content')
         <h1>Categories Page</h1>
+        <div class="row">
+
+            <div class="col l3 m4 s6 display-card">
+                <a href="signup.html" target="_blank" class="nav-link">
+                <div class="card cardHover">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img  img-fix src="{{URL('Images\Categories\clothing-re.jpg')}}" alt="image">
+                    </div>
+                    <div class="card-content">
+                        <span class="flow-text activator text-darken-4">
+                            Apparel
+                        </span>
+                    </div>
+                </div>      
+            </div><!--ending column-->
+
+            <div class="col l3 m4 s6 display-card">
+                <a href="signup.html" target="_blank" class="nav-link">
+                <div class="card cardHover">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img  img-fix src="{{URL('Images\Categories\books-re.jpg')}}" alt="image">
+                    </div>
+                    <div class="card-content">
+                        <span class="flow-text activator text-darken-4">
+                            Books
+                        </span>
+                    </div>
+                </div>      
+            </div><!--ending column-->
+
+            <div class="col l3 m4 s6 display-card">
+                <a href="signup.html" target="_blank" class="nav-link">
+                <div class="card cardHover">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator img-fix" src="{{URL('Images\Categories\electronics-re.jpg')}}" alt="image">
+                    </div>
+                    <div class="card-content">
+                        <span class="flow-text activator text-darken-4">
+                            Electronics
+                        </span>
+                    </div>
+                </div>      
+            </div><!--ending column-->
+
+            <div class="col l3 m4 s6 display-card">
+                <a href="signup.html" target="_blank" class="nav-link">
+                <div class="card cardHover">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img  img-fix src="{{URL('Images\Categories\fitness-re.jpg')}}" alt="image">
+                    </div>
+                    <div class="card-content">
+                        <span class="activator text-darken-4 flow-text">
+                            Fitness
+                        </span>
+                    </div>
+                </div>      
+            </div><!--ending column-->
+
+            <div class="col l3 m4 s6 display-card">
+                <a href="signup.html" target="_blank" class="nav-link">
+                <div class="card cardHover">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img  img-fix src="{{URL('Images\Categories\furniture-re.jpg')}}" alt="image">
+                    </div>
+                    <div class="card-content">
+                        <span class="flow-text activator text-darken-4">
+                            Furniture
+                        </span>
+                    </div>
+                </div>      
+            </div><!--ending column-->
+
+            <div class="col l3 m4 s6 display-card">
+                <a href="signup.html" target="_blank" class="nav-link">
+                <div class="card cardHover">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img  img-fix src="{{URL('Images\Categories\apparel.jpg')}}" alt="image">
+                    </div>
+                    <div class="card-content">
+                        <span class="flow-text activator text-darken-4">
+                            Clothing
+                        </span>
+                    </div>
+                </div>      
+            </div><!--ending column-->
+
+            <div class="col l3 m4 s6 display-card">
+                <a href="signup.html" target="_blank" class="nav-link">
+                <div class="card cardHover">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img  img-fix src="{{URL('Images\Categories\apparel.jpg')}}" alt="image">
+                    </div>
+                    <div class="card-content">
+                        <span class="flow-text activator text-darken-4">
+                            Clothing
+                        </span>
+                    </div>
+                </div>      
+            </div><!--ending column-->
+
+            <div class="col l3 m4 s6 display-card">
+                <a href="signup.html" target="_blank" class="nav-link">
+                <div class="card cardHover">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img  img-fix src="{{URL('Images\Categories\apparel.jpg')}}" alt="image">
+                    </div>
+                    <div class="card-content">
+                        <span class="flow-text activator text-darken-4">
+                            Clothing
+                        </span>
+                    </div>
+                </div>      
+            </div><!--ending column-->
+
+        </div><!--ending row-->
     @endsection
 </body>
 </html>
