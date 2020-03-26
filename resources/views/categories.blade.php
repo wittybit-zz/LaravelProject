54fa723491904f753779de693643da5a392af049
Ashutosh <ashutosh.empli5@gmail.com>
2020-03-26 18:32:43 +0530

    updated categories page with slider

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -24,27 +24,39 @@
     @section('title','Categories')
     @section('content')
         <h5 class="card-title">Categories Page</h5>
-        <div class="carousel carousel-slider center" style="width: 97vw;margin:auto">
-    <div class="carousel-fixed-item center">
-      <a class="btn waves-effect white grey-text darken-text-2">button</a>
-    </div>
-    <div class="carousel-item red white-text" href="#one!">
-      <h2>First Panel</h2>
-      <p class="white-text">This is your first panel</p>
-    </div>
-    <div class="carousel-item amber white-text" href="#two!">
-      <h2>Second Panel</h2>
-      <p class="white-text">This is your second panel</p>
-    </div>
-    <div class="carousel-item green white-text" href="#three!">
-      <h2>Third Panel</h2>
-      <p class="white-text">This is your third panel</p>
-    </div>
-    <div class="carousel-item blue white-text" href="#four!">
-      <h2>Fourth Panel</h2>
-      <p class="white-text">This is your fourth panel</p>
-    </div>
-  </div>
+        <div class="slider" style="width: 97vw;margin:10px 0;margin:10px auto;">
+    <ul class="slides">
+        <li>
+            <img src="http://lorempixel.com/580/250/nature/1"> <!-- random image -->
+            <div class="caption center-align">
+                <h3>This is our big Tagline!</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+        </li>
+        <li>
+            <img src="http://lorempixel.com/580/250/nature/2"> <!-- random image -->
+            <div class="caption left-align">
+                <h3>Left Aligned Caption</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+        </li>
+        <li>
+            <img src="http://lorempixel.com/580/250/nature/3"> <!-- random image -->
+            <div class="caption right-align">
+                <h3>Right Aligned Caption</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+        </li>
+        <li>
+            <img src="http://lorempixel.com/580/250/nature/4"> <!-- random image -->
+            <div class="caption center-align">
+                <h3>This is our big Tagline!</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+        </li>
+    </ul>
+</div>
+
         <div class="row">
             @foreach($data as $item)
             <div class="col l3 m4 s6 display-card">
@@ -62,13 +74,22 @@
             @endforeach
         </div><!--ending row-->
     @endsection
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript">
-          document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {
-        fullWidth: true,
-    indicators: true
-    });
-  });
+        $(document).ready(()=>{
+            $('.slider').slider({
+    full_width: true,
+    interval: 3000
+});
+
+$('.slider').slider('pause');
+
+$('#nextBtn').click(function(){
+    $('.slider').slider('start');
+    setTimeout(function(){
+        $('.slider').slider('pause');
+    },800);
+});
+        })
     </script>
 </div>
